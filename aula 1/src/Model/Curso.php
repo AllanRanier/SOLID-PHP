dc25c6cbec74e2821c1bc4719e7dcd78f0324126
AllanRanier <allanranier18@gmail.com>
2022-07-09 18:31:06 -0300

    Principios de SOLID em php

--- aula 1/src/Model/Curso.php
+++ aula 1/src/Model/Curso.php
@@ -2,7 +2,7 @@
 
 namespace Alura\Solid\Model;
 
-class Curso implements Pontuavel
+class Curso implements Pontuavel, Assistivel
 {
     private $nome;
     private $videos;
@@ -39,5 +39,12 @@ class Curso implements Pontuavel
     public function recuperarPontuacao(): int
     {
         return 100;
+    }
+
+    public function assistir(): void
+    {
+        foreach ($this->recuperarVideos() as $video) {
+            $this->assistir();
+        }
     }
 }
